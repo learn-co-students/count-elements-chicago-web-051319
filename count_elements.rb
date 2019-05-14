b2f86933e3f3ae89379987a2900d8d032caf91f3
Alex Hare <alexandra.f.hare@gmail.com>
2019-05-14 12:00:58 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,11 @@
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |word|
+    if new_hash.keys.include?(word)
+      new_hash[word] += 1 
+    else
+      new_hash[word] = 1
+    end
+  end
+  return new_hash
 end
- 
