f1f45307693873732d967bba16903a4a79f1d6ed
Martin Spicuzza <spicuzza157@gmail.com>
2019-05-15 01:48:25 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,16 @@
 def count_elements(array)
   # code goes here
+  my_hash = {}
+  array.each do |i|
+    if !my_hash.include?(i)
+      puts "I am here"
+      my_hash[i] = 1
+    else
+      puts "No no"
+      my_hash[i] = my_hash[i] + 1
+    end
+  end
+    
+  return my_hash
 end
  
